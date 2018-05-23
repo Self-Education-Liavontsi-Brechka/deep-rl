354424a0be896063ab7d13a6291c12c4fc723ed9
leontibrechko <leontibrechko@gmail.com>
2018-05-22 21:50:43 -0700

    fixed the training flow

--- hw1/behavioral_cloning.py
+++ hw1/behavioral_cloning.py
@@ -13,10 +13,12 @@ def parse_args():
     parser.add_argument('envname', type=str)
     parser.add_argument('--render', action='store_true')
     parser.add_argument("--max_timesteps", type=int)
-    parser.add_argument('--num_rollouts', type=int, default=20,
+    parser.add_argument('--num_episodes', type=int, default=20,
                         help='Number of expert roll outs')
-    parser.add_argument('--num_test_rollouts', type=int, default=5,
+    parser.add_argument('--num_test_episodes', type=int, default=5,
                         help='Number of test roll outs for target policy')
+    parser.add_argument('--batch_size', type=int, default=128)
+    parser.add_argument('--num_epochs', type=int, default=20)
     parser.add_argument('--summary_dir', type=str, help='Direction to write summaries')
     return parser.parse_args()
 
@@ -62,8 +64,8 @@ def build_target_policy(env):
         return session.run(action_output, {observation_input: observation})
 
     def update_fn(session, observation, label):
-        loss_result, _, global_step = session.run(
-            [loss, train_optimization, tf.train.get_global_step()],
+        _, loss_result, global_step = session.run(
+            [train_optimization, loss, tf.train.get_global_step()],
             {
                 observation_input: observation,
                 label_input: label
@@ -84,50 +86,68 @@ def create_summary_writer(session, env, summary_dir=None):
     return tf.summary.FileWriter(summary_dir, session.graph) if summary_dir else None
 
 
-def clone_behaviour(session, env, expert_policy, num_episodes=10, max_timesteps=None, summary_dir=None, render=None):
+def clone_behaviour(session, env, expert_policy, batch_size, num_epochs, num_episodes, max_timesteps=None,
+                    summary_dir=None):
+    max_timesteps = max_timesteps or env.spec.timestep_limit
+    transitions = []
+    rewards = []
+
+    for i_episode in range(num_episodes):
+        total_reward = 0.0
+
+        o = env.reset()
+        for t in itertools.count():
+            expert_policy_action = np.squeeze(expert_policy(np.expand_dims(o, 0)))
+            o_prime, reward, done, _ = env.step(expert_policy_action)
+
+            transitions.append((o, expert_policy_action))
+
+            total_reward += reward
+            o = o_prime
+
+            if t % max(1, max_timesteps // 10) == 0:
+                print('\rEpisode {}/{}: step {}'.format(i_episode + 1, num_episodes, t), end='')
+
+            if done or t >= max_timesteps:
+                break
+
+        rewards.append(total_reward)
+
+    rewards_mean = np.mean(rewards)
+    rewards_deviation = np.sqrt(np.mean((rewards - rewards_mean) ** 2))
+    print('\nExpert Rewards Mean: {}, Expert Rewards deviation: {}'.format(rewards_mean, rewards_deviation))
+
     target_policy, update_target_policy = build_target_policy(env)
     summary_writer = create_summary_writer(session, env, summary_dir)
 
     session.run(tf.global_variables_initializer())
 
-    max_timesteps = max_timesteps or env.spec.timestep_limit
-    rewards = []
+    transitions = np.array(transitions)
 
-    for i_episode in range(num_episodes):
+    for i_epoch in range(num_epochs):
         losses = []
-        total_reward = 0.0
 
-        o = np.expand_dims(env.reset(), 0)
-        for t in itertools.count():
-            expert_policy_action = expert_policy(o)
-            o_prime, reward, done, _ = env.step(expert_policy_action)
+        np.random.shuffle(transitions)
 
-            losses.append(update_target_policy(session, o, np.array(expert_policy_action)))
-            total_reward += reward
+        t = 0
+        T = len(transitions)
+        while t < T:
+            o_batch, a_batch = map(np.array, zip(*transitions[t: min(t + batch_size, T)]))
 
-            o = np.expand_dims(o_prime, 0)
+            losses.append(update_target_policy(session, o_batch, a_batch))
 
-            if t % max(1, max_timesteps // 10) == 0:
-                print('\rEpisode {}/{}: step {}'.format(i_episode + 1, num_episodes, t), end='')
+            print('\rTimestep {}/{}'.format(t + 1, T), end='')
 
-            if render:
-                env.render()
-            if done or t >= max_timesteps:
-                break
+            t += batch_size
 
-        rewards.append(total_reward)
         loss_mean = np.mean(losses)
 
         if summary_writer:
             summary = tf.Summary()
-            summary.value.add(simple_value=loss_mean, tag='episode/loss_mean')
-            summary_writer.add_summary(summary, i_episode)
+            summary.value.add(simple_value=loss_mean, tag='epoch/loss_mean')
+            summary_writer.add_summary(summary, i_epoch)
 
-        print('\nAverage loss for the episode: {}'.format(loss_mean))
-
-    rewards_mean = np.mean(rewards)
-    rewards_deviation = np.sqrt(np.mean((rewards - rewards_mean) ** 2))
-    print('Expert Rewards Mean: {}, Expert Rewards deviation: {}'.format(rewards_mean, rewards_deviation))
+        print('\nAverage loss for the epoch: {}'.format(loss_mean))
 
     return target_policy
 
@@ -173,11 +193,11 @@ def main():
     env = gym.make(args.envname)
 
     with tf.Session() as session:
-        target_policy = clone_behaviour(session, env, expert_policy, num_episodes=args.num_rollouts,
-                                        max_timesteps=args.max_timesteps,
-                                        summary_dir=args.summary_dir, render=args.render)
+        target_policy = clone_behaviour(session, env, expert_policy, batch_size=args.batch_size,
+                                        num_epochs=args.num_epochs, num_episodes=args.num_episodes,
+                                        max_timesteps=args.max_timesteps, summary_dir=args.summary_dir)
 
-        test_target_policy(session, env, target_policy, args.num_test_rollouts, args.max_timesteps, args.render)
+        test_target_policy(session, env, target_policy, args.num_test_episodes, args.max_timesteps, args.render)
 
 
 if __name__ == '__main__':
