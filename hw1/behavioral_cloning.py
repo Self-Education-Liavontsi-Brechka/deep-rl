c603352c73ac8f3a7b02f82ed7c4e6b5209ad028
leontibrechko <leontibrechko@gmail.com>
2018-05-22 19:51:59 -0700

    better summary generation

--- hw1/behavioral_cloning.py
+++ hw1/behavioral_cloning.py
@@ -30,63 +30,61 @@ def load_expert_policy(args):
     return policy_fn
 
 
-def build_target_policy(env, summary_writer=None):
+def build_target_policy(env):
     global_step = tf.Variable(0, name='global_step', trainable=False)
 
     layer1_units = 64
     layer2_units = 64
 
     # ----------------------- PREDICT PART ------------------------
-    observation_input = tf.placeholder(tf.float32, [None, env.observation_space.shape[0]], 'observation-input')
+    with tf.name_scope('prediction'):
+        observation_input = tf.placeholder(tf.float32, [None, env.observation_space.shape[0]], 'observation-input')
 
-    current_activations = tf.layers.dense(observation_input, layer1_units, tf.nn.tanh, name='hidden-layer-1')
-    current_activations = tf.layers.dense(current_activations, layer2_units, tf.nn.tanh, name='hidden-layer-2')
-    action_output = tf.layers.dense(current_activations, env.action_space.shape[0], name='action-output')
+        current_activations = tf.layers.dense(observation_input, layer1_units, tf.nn.tanh, name='hidden-layer-1')
+        current_activations = tf.layers.dense(current_activations, layer2_units, tf.nn.tanh, name='hidden-layer-2')
+        action_output = tf.layers.dense(current_activations, env.action_space.shape[0], name='action-output')
 
-    # ----------------------- LEARN PART --------------------------
-    label_input = tf.placeholder(tf.float32, [None, env.action_space.shape[0]], 'label-input')
+    # ----------------------- TRAIN PART --------------------------
+    with tf.name_scope('training'):
+        label_input = tf.placeholder(tf.float32, [None, env.action_space.shape[0]], 'label-input')
 
-    loss = tf.losses.mean_squared_error(label_input, action_output)
-    optimizer = tf.train.AdamOptimizer()
-    train_optimization = optimizer.minimize(loss, tf.train.get_global_step())
-
-    # ----------------------- DEBUG PART --------------------------
-    summaries = tf.summary.merge([
-        tf.summary.scalar('loss_per_timestep', loss)
-    ])
+        loss = tf.losses.mean_squared_error(label_input, action_output)
+        optimizer = tf.train.AdamOptimizer()
+        train_optimization = optimizer.minimize(loss, tf.train.get_global_step())
 
     def predict_fn(session, observation):
         return session.run(action_output, {observation_input: observation})
 
     def update_fn(session, observation, label):
-        loss_result, _, summaries_result, global_step = session.run(
-            [loss, train_optimization, summaries, tf.train.get_global_step()],
+        loss_result, _, global_step = session.run(
+            [loss, train_optimization, tf.train.get_global_step()],
             {
                 observation_input: observation,
                 label_input: label
             }
         )
 
-        if summary_writer:
-            summary_writer.add_summary(summaries_result, global_step)
-
         return loss_result
 
     return predict_fn, update_fn
 
 
-def clone_behaviour(session, env, expert_policy, num_episodes=10,
-                    max_timesteps=None, summary_dir=None, render=None):
+def create_summary_writer(session, env, summary_dir=None):
     if summary_dir:
         summary_dir = os.path.abspath("./{}/{}/{}".format(summary_dir, env.spec.id, time.strftime("%Y-%m-%d-%H-%M-%S")))
         if not os.path.exists(summary_dir):
             os.makedirs(summary_dir)
 
-    summary_writer = tf.summary.FileWriter(summary_dir) if summary_dir else None
-    predict, update_target_policy = build_target_policy(env, summary_writer)
-    max_timesteps = max_timesteps or env.spec.timestep_limit
+    return tf.summary.FileWriter(summary_dir, session.graph) if summary_dir else None
+
+
+def clone_behaviour(session, env, expert_policy, num_episodes=10, max_timesteps=None, summary_dir=None, render=None):
+    target_policy, update_target_policy = build_target_policy(env)
+    summary_writer = create_summary_writer(session, env, summary_dir)
 
     session.run(tf.global_variables_initializer())
+
+    max_timesteps = max_timesteps or env.spec.timestep_limit
 
     for i_episode in range(num_episodes):
         losses = []
@@ -96,8 +94,7 @@ def clone_behaviour(session, env, expert_policy, num_episodes=10,
             expert_policy_action = expert_policy(o)
             o_prime, reward, done, _ = env.step(expert_policy_action)
 
-            loss = update_target_policy(session, o, np.array(expert_policy_action))
-            losses.append(loss)
+            losses.append(update_target_policy(session, o, np.array(expert_policy_action)))
 
             o = np.expand_dims(o_prime, 0)
 
@@ -109,16 +106,16 @@ def clone_behaviour(session, env, expert_policy, num_episodes=10,
             if done or t >= max_timesteps:
                 break
 
-        average_loss = session.run(tf.reduce_mean(losses))
+        average_loss = np.mean(losses)
 
         if summary_writer:
             summary = tf.Summary()
-            summary.value.add(simple_value=average_loss, tag='average_loss_per_episode')
+            summary.value.add(simple_value=average_loss, tag='episode/loss')
             summary_writer.add_summary(summary, i_episode)
 
         print('\nAverage loss for the episode: {}'.format(average_loss))
 
-    return predict
+    return target_policy
 
 
 def main():
